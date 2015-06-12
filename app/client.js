3b6210e29922711bfabfe6c5bb4f64b4ac384108
Lady Blue <bluelady.2015@yahoo.com>
2015-06-11 20:48:07 -0700

    Add adjective module and random name generator

--- app/client.js
+++ app/client.js
@@ -1,21 +1,53 @@
 $(function() {
-	console.log("Hi!");
+	$("#submitWords").on("submit", function(e) {
+		e.preventDefault();
+		var adjective = $("input[name=adjective]").val();
+		var adjPost;
+
+		if (adjective) {
+			adjPost = {word: adjective};
+			$.post("adjective", adjPost, function(response) {
+				var adjectiveRes = response.msg;
+				var confirm = response.message;
+				$("#adjectiveRes").html(confirm);
+			});
+		}
+	});
+
+	$("#name").click(function() {
+		$.get("adjective", function(response) {
+			var adjective = response.word;
+			$("#adjective").text(adjective);
+		});
+
+		$.get("verb", function(response) {
+			var verb = response.word;
+			$("#verb").text(verb);
+		});
+
+		$.get("noun", function(response) {
+			var noun = response.word;
+			$("#noun").text(noun);
+		});
+	});
+	
 	$("#adj").on("click", function() {
-		$.get("http://localhost:3000/adjective", function(response) {
+		console.log("Hi!");
+		$.get("http://localhost:3000/adjectives", function(response) {
 			$("#ajax-text").html(response);
 		});
 	});
 
-	$("#verb").on("click", function() {
+	$("#verby").on("click", function() {
 		console.log("Hello!");
-		$.get("http://localhost:3000/verb", function(response) {
+		$.get("http://localhost:3000/verbs", function(response) {
 			$("#verbtext").html(response);
 		});
 	});
 
-	$("#noun").on("click", function() {
+	$("#nouny").on("click", function() {
 		console.log("Hey!");
-		$.get("http://localhost:3000/noun", function(response) {
+		$.get("http://localhost:3000/nouns", function(response) {
 			$("#nountext").html(response);
 		});
 	});
--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var port = process.env.PORT || 3000;
+var Adjective = require('./lib/adjective');
+var getRandomWord = require('./lib/getRandomWord');
 
-var myArray = ["String One", "String Two", "String Three"];
-var returnRandomString = function(array) {
-	var getRandomNumber = Math.floor(Math.random() * array.length);
-	return array[getRandomNumber];
+var adjective = new Adjective();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(express.static(__dirname + "/app"));
+
+var Verb = function() {
+	this.sleeping = true;
+	this.skiing = true;
+	this.flopped = true;
+	this.waiting = true;
+	this.plotting = true;
+	this.burping = true;
+	this.sneezing = true;
+	this.flying = true;
+	this.snoozing = true;
+	this.dancing = true;
 };
+var verb = new Verb();
+
+var Noun = function() {
+	this.ninjas = true;
+	this.cowboys = true;
+	this.bears = true;
+	this.beats = true;
+	this.alligators = true;
+	this.hamsters = true;
+	this.otters = true;
+	this.helicopters = true;
+	this.unicorns = true;
+	this.rhymes = true;
+};
+var noun = new Noun();
 
 var adjectives = [
 "awesome",
@@ -22,13 +56,13 @@ var returnRandomAdjective = function(adjectives) {
 };
 
 var verbs = [
-"run", 
-"jump",
-"sing",
-"dance",
-"program",
-"eat",
-"sleep"
+"running", 
+"jumping",
+"singing",
+"dancing",
+"programming",
+"eating",
+"sleeping"
 ];
 var returnRandomVerb = function(verbs) {
 	var getRandomVerb = Math.floor(Math.random() * verbs.length);
@@ -48,22 +82,48 @@ var returnRandomNoun = function(nouns) {
 	return nouns[getRandomNoun];
 };
 
+var myArray = ["String One", "String Two", "String Three"];
+var returnRandomString = function(array) {
+	var getRandomNumber = Math.floor(Math.random() * array.length);
+	return array[getRandomNumber];
+};
+
+function postWord(word, wordObject) {
+	var msg;
+	if (wordObject.hasOwnProperty(word)) {
+		msg = "We already have your awesome word: " + word;
+	} else {
+		wordObject[word] = true;
+		console.dir(wordObject);
+		msg = "Thanks for submitting " + word + "!";
+	}
+	return {message: msg, confirm: word}
+}
+
 app.get("/adjective", function (req, res) {
-	res.send(returnRandomAdjective(adjectives));
+	res.json(getRandomWord(adjective));
 });
 
 app.get("/verb", function (req, res) {
-	res.send(returnRandomVerb(verbs));
+	res.json(getRandomWord(verb));
 });
 
 app.get("/noun", function (req, res) {
+	res.json(getRandomWord(noun));
+});
+
+app.get("/adjectives", function (req, res) {
+	res.send(returnRandomAdjective(adjectives));
+});
+
+app.get("/verbs", function (req, res) {
+	res.send(returnRandomVerb(verbs));
+});
+
+app.get("/nouns", function (req, res) {
 	res.send(returnRandomNoun(nouns));
 });
-
-app.listen(port, function() {
-	console.log('server starting. available at http://localhost:' + port);
-});
-
+/*
 app.get("/hello", function (req, res) {
 	res.send("hello universe");
 });
@@ -75,7 +135,21 @@ app.get("/randomstring", function (req, res) {
 app.get("/", function (req, res) {
 	res.sendFile(__dirname + "/app/index.html");
 });
+*/
+app.post("/adjective", function (req, res) {
+	res.json(postWord(req.body.word, adjective));
+});
 
-app.use(express.static(__dirname + "/app"));
+app.get("/", function (req, res) {
+	res.sendFile("index.html");
+});
+
+app.listen(port, function() {
+	console.log('server starting. available at http://localhost:' + port);
+});
+
+
+
+
 
 
